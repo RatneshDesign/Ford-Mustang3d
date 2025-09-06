3bc776a3e04f451241cae94795137185114eede7
ratneshkhus <ratneshkumawat755@gmail.com>
2025-09-06 21:54:22 +0530

    issue solved after waiting fot the holiday

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,101 +10,143 @@ function Model({ url, scale = 1 }) {
     const { scene } = useGLTF(url);
     const groupRef = useRef();
 
+    // useLayoutEffect(() => {
+    //     if (!groupRef.current) return;
+
+    //     const ctx = gsap.context(() => {
+    //         // Rotate as you scroll
+    //         //   gsap.to(groupRef.current.rotation, {
+    //         //     y: Math.PI * 2,
+    //         //     scrollTrigger: {
+    //         //       trigger: "#scroll-section",
+    //         //       start: "top top",
+    //         //       end: "bottom bottom",
+    //         //       scrub: true,
+    //         //     },
+    //         //   });
+
+    //         // Move forward slightly
+    //         gsap.to(groupRef.current.position, {
+    //             x: 3.5,
+    //             scrollTrigger: {
+    //                 trigger: "#section1",
+    //                 start: "top bottom",
+    //                 end: "bottom bottom",
+    //                 scrub: true,
+    //                 markers: true,
+    //             },
+    //         }, 'about');
+    //         gsap.to(groupRef.current.rotation, {
+    //             x: -0.2,
+    //             y: -0.4,
+    //             z: 0.1,
+    //             scrollTrigger: {
+    //                 trigger: "#section1",
+    //                 start: "top bottom",
+    //                 end: "bottom bottom",
+    //                 scrub: true,
+    //                 markers: true,
+    //             },
+    //         }, 'about');
+
+    //         gsap.to(groupRef.current.position, {
+    //             x: 1,
+    //             z: 1,
+    //             scrollTrigger: {
+    //                 trigger: "#section2",
+    //                 start: "top bottom",
+    //                 end: "bottom bottom",
+    //                 scrub: true,
+    //                 markers: true,
+    //             },
+    //         }, 'topview');
+    //         gsap.to(groupRef.current.rotation, {
+    //             x: 1,
+    //             y: 1.69,
+    //             // z: 0.1,
+    //             scrollTrigger: {
+    //                 trigger: "#section2",
+    //                 start: "top bottom",
+    //                 end: "bottom bottom",
+    //                 scrub: true,
+    //                 markers: true,
+    //             },
+    //         }, 'topview');
+
+    //         gsap.to(groupRef.current.position, {
+    //             x: -4,
+    //             y: 0.4,
+    //             z: 3,
+    //             scrollTrigger: {
+    //                 trigger: "#section3",
+    //                 start: "top bottom",
+    //                 end: "bottom bottom",
+    //                 scrub: true,
+    //                 markers: true,
+    //             },
+    //         }, 'tofrontview');
+    //         gsap.to(groupRef.current.rotation, {
+    //             // x: 1.2,
+    //             // y: 1.69,
+    //             // z: -2,
+    //             x: 1,
+    //             y: 1.69,
+    //             z: 0.3,
+    //             scrollTrigger: {
+    //                 trigger: "#section3",
+    //                 start: "top bottom",
+    //                 end: "bottom bottom",
+    //                 scrub: true,
+    //                 markers: true,
+    //             },
+    //         }, 'tofrontview');
+
+    //     });
+
+    //     return () => ctx.revert();
+    // }, []);
+
     useLayoutEffect(() => {
         if (!groupRef.current) return;
 
         const ctx = gsap.context(() => {
-            // Rotate as you scroll
-            //   gsap.to(groupRef.current.rotation, {
-            //     y: Math.PI * 2,
-            //     scrollTrigger: {
-            //       trigger: "#scroll-section",
-            //       start: "top top",
-            //       end: "bottom bottom",
-            //       scrub: true,
-            //     },
-            //   });
+            let tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#scroll-section",
+                    start: "top top",
+                    end: "bottom bottom",
+                    scrub: true,
+                    markers: true,
+                    pin: "#canvas-container",
+                }
+            });
 
-            // Move forward slightly
-            gsap.to(groupRef.current.position, {
-                x: 3.5,
-                scrollTrigger: {
-                    trigger: "#section1",
-                    start: "top bottom",
-                    end: "bottom bottom",
-                    scrub: true,
-                    markers: true,
-                },
-            }, 'about');
-            gsap.to(groupRef.current.rotation, {
-                x: -0.2,
-                y: -0.4,
-                z: 0.1,
-                scrollTrigger: {
-                    trigger: "#section1",
-                    start: "top bottom",
-                    end: "bottom bottom",
-                    scrub: true,
-                    markers: true,
-                },
-            }, 'about');
+            tl.add("section1")
+                .to(groupRef.current.position, { x: 3.5, duration: 1 }, "section1")
+                .to(groupRef.current.rotation, { x: -0.2, y: -0.4, z: 0.1, duration: 1 }, "section1")
 
-            gsap.to(groupRef.current.position, {
-                x: 1,
-                z: 1,
-                scrollTrigger: {
-                    trigger: "#section2",
-                    start: "top bottom",
-                    end: "bottom bottom",
-                    scrub: true,
-                    markers: true,
-                },
-            }, 'topview');
-            gsap.to(groupRef.current.rotation, {
-                x: 1,
-                y: 1.69,
-                // z: 0.1,
-                scrollTrigger: {
-                    trigger: "#section2",
-                    start: "top bottom",
-                    end: "bottom bottom",
-                    scrub: true,
-                    markers: true,
-                },
-            }, 'topview');
+                .add("section2")
+                .to(groupRef.current.position, { x: 1, Y: 1, z: 1, duration: 1 }, "section2")
+                .to(groupRef.current.rotation, { x: 1, y: 1.69, duration: 1 }, "section2")
 
-            gsap.to(groupRef.current.position, {
-                x: -4,
-                y: -0,
-                z: 3,
-                scrollTrigger: {
-                    trigger: "#section3",
-                    start: "top bottom",
-                    end: "bottom bottom",
-                    scrub: true,
-                    markers: true,
-                },
-            },'tofrontview');
-            gsap.to(groupRef.current.rotation, {
-                // x: 1.2,
-                // y: 1.69,
-                // z: -2,
-                x: 1,
-                y: 1.69,
-                z: 0.3,
-                scrollTrigger: {
-                    trigger: "#section3",
-                    start: "top bottom",
-                    end: "bottom bottom",
-                    scrub: true,
-                    markers: true,
-                },
-            }, 'tofrontview');
+                .add("section3")
+                .to(groupRef.current.position, { x: -4, y: 0.4, z: 3, duration: 1 }, "section3")
+                .to(groupRef.current.rotation, { x: 1, y: 1.69, z: 0.3, duration: 1 }, "section3")
 
+                .add("section4")
+                .to(groupRef.current.position, { x: -0.5, y: -0.5, z: -2, duration: 1 }, "section4")
+                .to(groupRef.current.rotation, { x: -0.1, y: 0.3, z: 0.02, duration: 1 }, "section4")
+       
+                .add("section5")
+                .to(groupRef.current.position, { x: -0.5, y: -0.5, z: -2, duration: 1 }, "section5")
+                .to(groupRef.current.rotation, { x: -0.1, y: 0.3, z: 0.02, duration: 1 }, "section5");
+       
+                // y    // z        // x
         });
 
         return () => ctx.revert();
     }, []);
+
 
     return (
         <group ref={groupRef} scale={scale} position={[2.1, -0.6, -2]} rotation={[-0.1, -0.5, 0]}>
@@ -115,54 +157,64 @@ function Model({ url, scale = 1 }) {
 
 function Home() {
     return (
-        <div id="scroll-section" style={{ height: "500vh" }}>
-            <span className="flex absolute text-[25rem] w-screen h-screen items-center justify-center">
-                <h1 className="font-bold z-[-1]">She</h1>
-                <h1 className="font-bold z-10">lbY</h1>
-                <span className="text-2xl absolute bottom-[20px] tracking-[30px] ">Ford</span>
-            </span>
-            <div
-                id="canvas-container"
-                style={{
-                    position: "sticky",
-                    top: 0,
-                    width: "100%",
-                    height: "100vh",
-                }}
-                className="flex items-center justify-center"
-            >
+        <>
+
+            <div id="scroll-section" style={{ height: "500vh" }} className="w-screen flex flex-col">
+
+                <span className={`herofont flex absolute text-[25rem] w-screen h-screen items-center justify-center`}>
+                    <h1 className="font-bold z-[-1]">She</h1>
+                    <h1 className="font-bold z-10">lby</h1>
+                    <span className="text-2xl absolute bottom-[20px] tracking-[30px] ">Ford</span>
+                </span>
+
+                <div
+                    id="canvas-container"
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100vh",
+                    }}
+                    className="flex items-center justify-center"
+                >
 
 
-                <Canvas camera={{ position: [2, 2, 6], fov: 50 }} shadows>
-                    <Suspense fallback={null}>
-                        <ambientLight intensity={0.5} />
-                        <directionalLight
-                            castShadow
-                            intensity={1.2}
-                            position={[5, 10, 5]}
-                            shadow-mapSize-width={2048}
-                            shadow-mapSize-height={2048}
-                        />
-                        <hemisphereLight intensity={0.35} />
+                    <Canvas camera={{ position: [2, 2, 6], fov: 50 }} shadows>
+                        <Suspense fallback={null}>
+                            <ambientLight intensity={0.5} />
+                            <directionalLight
+                                castShadow
+                                intensity={1.2}
+                                position={[5, 10, 5]}
+                                shadow-mapSize-width={2048}
+                                shadow-mapSize-height={2048}
+                            />
+                            <hemisphereLight intensity={0.35} />
 
-                        <Model url="/ford_mustang_shelby.glb" scale={1.2} />
+                            <Model url="/ford_mustang_shelby.glb" scale={1.2} />
 
-                        <Environment preset="city" />
-                        <OrbitControls enableZoom={false} />
-                    </Suspense>
-                </Canvas>
+                            <Environment preset="city" />
+                            <OrbitControls enableZoom={false} />
+                        </Suspense>
+                    </Canvas>
+                </div>
+
+                <div className="w-screen h-screen"></div>
+                <section id="section1" className="flex h-screen bg-amber-300 justify-start items-center px-[5vw]">
+                    <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
+                </section>
+                <section id="section2" className="flex h-screen bg-amber-600 justify-start items-center px-[5vw]">
+                    <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
+                </section>
+                <section id="section3" className="flex h-screen bg-amber-800 justify-start items-center px-[5vw]">
+                    <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
+                </section>
+                <section id="section4" className="flex h-screen bg-amber-900 justify-start items-center px-[5vw]">
+                    <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
+                </section>
             </div>
-
-            <section id="section1" className="flex h-screen bg-amber-300 justify-start items-center px-[5vw]">
-                <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
-            </section>
-            <section id="section2" className="flex h-screen bg-amber-600 justify-start items-center px-[5vw]">
-                <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
-            </section>
-            <section id="section3" className="flex h-screen bg-amber-800 justify-start items-center px-[5vw]">
-                <span className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iure esse doloribus iste impedit fuga tempore voluptates culpa temporibus aspernatur.</span>
-            </section>
-        </div>
+        </>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-// tailwind.config.js
-export default {
+// tailwind.config.cjs
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
     extend: {
       fontFamily: {
-        lexend: ["Lexend", "sans-serif"],
-      },
-    },
+        lexend: ['Lexend', 'sans-serif'],
+        ptsans: ['"PT Sans Narrow"', 'sans-serif'],
+      }
+    }
   },
+  plugins: [],
 };
